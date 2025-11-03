7a01acc720679d61cb3ba5d5aec67616100b9228
skmasum98 <skmasum98@gmail.com>
2025-11-04 02:03:52 +0600

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -43,7 +43,7 @@ const sendEmail = async (options) => {
         process.env.NODE_ENV === "production" ? email : process.env.EMAIL_SERVICE_USER;
 
       const resendResponse = await resend.emails.send({
-        from: `Blog App <onboarding@resend.dev>`, 
+        from: `Blog App <${process.env.EMAIL_SERVICE_USER}>`, 
         to: resendTo,
         subject,
         html: message,
